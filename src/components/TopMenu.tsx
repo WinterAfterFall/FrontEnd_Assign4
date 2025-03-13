f03d1948009b2346045fa3ce70a6bfb8cecb838d
Natthaphat Jitthaworn <tonnam.jit@gmail.com>
2025-03-13 16:50:25 +0700

    Complete

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,7 +3,7 @@ import TopMenuItem from "./TopMenuItem";
 
 const TopMenu = () => {
   return (
-    <div className="bg-gray-800 p-4 flex justify-end items-center ">
+    <div className="bg-gray-800 p-4 flex justify-end items-center text-white">
       <div className="flex items-center gap-8"> {/* ใช้ gap */}
       <TopMenuItem label="Booking" href="/booking" />
         <img src="/img/logo.png" alt="Logo" className="h-10" />
